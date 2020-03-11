be9b61ea847e00e32934e92418af8f469207cb00
Anna Dybas <a.m.dybas@gmail.com>
2020-03-11 10:08:35 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(n)
+    (1...n).select{|num| (num % 3===0) || (num % 5===0)}
+end
+
+def sum_multiples(n)
+    collect_multiples(n).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :number
+
+    def initialize(number)
+        @number = number
+    end
+
+    def collect_multiples()
+        (1...number).select{|num| (num % 3===0) || (num % 5===0)}
+    end
+    
+    def sum_multiples()
+        collect_multiples().reduce(:+)
+    end
+
+end
